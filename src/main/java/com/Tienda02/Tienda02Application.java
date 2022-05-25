814653a01529bac40f0d0b312942422e9d6d5e73
Usuario <Usuario@SofiLoaiza>
2022-05-24 21:06:34 -0600

    Pimer commit del proyecto tienda02

---
+++ src/main/java/com/Tienda02/Tienda02Application.java
@@ -0,0 +1,13 @@
+package com.Tienda02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tienda02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tienda02Application.class, args);
+	}
+
+}
